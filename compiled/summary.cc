b9343e1f2c49bd8952d5449d27023e45df2c93f4
liebercanis <mgold@unm.edu>
2023-02-23 16:12:23 -0700

    summary

--- compiled/summary.cc
+++ compiled/summary.cc
@@ -129,8 +129,7 @@ int main(int argc, char *argv[])
   for (unsigned ic = 0; ic < nchan; ++ic)
     printf(" chan %u vecQsum %lu  \n", ic, vecQsum[ic].size());
   for (unsigned ifile = 0; ifile < vecQsum[7].size(); ++ifile )
-    printf("  summary-channel-6 %u %f  ",ifile, vecQsum[7][ifile]);
-  cout << endl;
+    printf("  summary file %u  %s chan6 %f  \n", ifile, fileList[ifile].Data(), vecQsum[7][ifile]);
 
   // one graph per channel
   vector<TGraphErrors *> gqsum;
