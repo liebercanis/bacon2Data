a26188f3e8e1d5981ee51eefb0e31101b92d2eb3
liebercanis <mgold@unm.edu>
2023-03-16 16:41:42 -0600

    time axis

--- compiled/summary.cc
+++ compiled/summary.cc
@@ -444,7 +444,7 @@ int main(int argc, char *argv[])
   mg->GetXaxis()->SetTimeDisplay(1);
   //mg->GetXaxis()->SetNdivisions(1010);
   mg->GetXaxis()->SetNdivisions(-219);
-  mg->GetXaxis()->SetTimeFormat("%Y-%m-%d %H-%M");
+  mg->GetXaxis()->SetTimeFormat("%d:%H");
   mg->GetXaxis()->SetTimeOffset(0, "gmt");
   mg->GetYaxis()->SetTitle("integrated charge (normed)");
   TCanvas *can = new TCanvas("Qsummary", "Qsummary");
@@ -458,7 +458,7 @@ int main(int argc, char *argv[])
   int ndiv = 10 + 100 * 5 + 10000 * 3;
   mgQPE->GetXaxis()->SetNdivisions(ndiv);
   mgQPE->GetXaxis()->SetNdivisions(-219);
-  mgQPE->GetXaxis()->SetTimeFormat("%d %H");
+  mgQPE->GetXaxis()->SetTimeFormat("%d:%H");
   mgQPE->GetXaxis()->SetTimeOffset(0, "gmt");
   mgQPE->GetYaxis()->SetTitle(" single photon charge (normed)");
   TCanvas *canqpe = new TCanvas("QPE", "QPE");
