1eebf706635feaf40e952370394c4c305174a5eb
liebercanis <mgold@unm.edu>
2023-03-29 14:10:00 -0600

    update method

--- bobj/TBFile.cxx
+++ bobj/TBFile.cxx
@@ -11,13 +11,19 @@ TBFile::TBFile(): TNamed("tbfile","none")
 
 TBFile::TBFile(TString name): TNamed(TString("tbfile"),name)
 {
+  update(name);
+}
+
+void TBFile::update(TString name){
   struct stat fileInfo;
   if (stat(name, &fileInfo) != 0)
   { // Use stat() to get the info
-    std::cout << "Error: " << strerror(errno) << std::endl;
+    std::cout << "Error: " << strerror(errno) << " using stat for file " << name << std::endl;
     return;
   }
-  created= std::ctime(&fileInfo.st_ctime); // Creation time
+  tsc = TTimeStamp(fileInfo.st_ctime);
+  tsm = TTimeStamp(fileInfo.st_mtime);
+  created = std::ctime(&fileInfo.st_ctime); // Creation time
   modified = std::ctime(&fileInfo.st_mtime);
   print();
 }
--- bobj/TBFile.hxx
+++ bobj/TBFile.hxx
@@ -5,7 +5,9 @@
 #define TBFILE_DEFINED
 #include <iostream>
 #include <string>
+#include <time.h>
 #include<TNamed.h>
+#include<TTimeStamp.h>
 using namespace std;
 // class to store file info
 
@@ -14,15 +16,20 @@ class TBFile : public TNamed
 public:
   TBFile();
   TBFile(TString name);
+  void update(TString name);
+  
   //~TBFile();
   string created;
   string modified;
-  
-  void print()
+  TTimeStamp tsm; // modified time in seconds
+  TTimeStamp tsc; // created time in seconds
+
+  void
+  print()
   {
     cout << " TBFile  " << this->GetTitle() << endl << " created " << created << " modified " << modified << endl;
   }
 
-  ClassDef(TBFile, 1)
+  ClassDef(TBFile, 2)
 };
 #endif
