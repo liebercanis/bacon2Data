d971a75b30891c1f83e8bd45b986b22126de2b11
liebercanis <mgold@unm.edu>
2024-05-16 17:22:54 -0600

    no print

--- compiled/anaCRun.cc
+++ compiled/anaCRun.cc
@@ -1345,7 +1345,7 @@ Long64_t anaCRun::anaCRunFile(TString theFile, Long64_t maxEntries, Long64_t fir
     // printf(" event %lld passbit %x bin 0 %f \n",entry, passBit,hEventPass->GetBinContent(0));
     //  if(eventPass!=0)
     //    printf("event fails with eventPass = %x npass %i nfail %i \n", eventPass,npass,nfail);
-    tbrun->print();
+    //tbrun->print();
     tbrun->fill();
   }
   printf(" \n \n At END OF FILE total pass  = %i fail %i  \n", npass, nfail);
