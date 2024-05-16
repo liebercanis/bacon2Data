60e660a386fbfdf3af531db136386c75dae44b32
liebercanis <mgold@unm.edu>
2024-05-16 17:52:52 -0600

    debug

--- bobj/hitFinder.cxx
+++ bobj/hitFinder.cxx
@@ -908,7 +908,7 @@ void hitFinder::makeHits(int idet, Double_t &triggerTime, Double_t &firstCharge)
       {
         used = true;
         if (verbose)
-          printf("line887 hitFinder::makeHit found multiple hits det %i this (%i,%i.%i) last peak (%i,%i,%i) dethit size %lu \n", idet, hiti.firstBin, hiti.peakBin, hiti.lastBin, dhit.firstBin, dhit.peakBin, dhit.lastBin, detHits.size());
+          printf("line887 hitFinder::makeHit found multiple hits det %i klow %i khigh %i  this (%i,%i,%i) last peak (%i,%i,%i) dethit size %lu \n", idet, klow, khigh, hiti.firstBin, hiti.peakBin, hiti.lastBin, dhit.firstBin, dhit.peakBin, dhit.lastBin, detHits.size());
       }
     }
     ntFinder->Fill(float(theEvent), float(idet), float(detHits.size()), float(dhit.firstBin), float(dhit.startTime), float(dhit.peakBin), float(dhit.lastBin), dhit.qpeak);
