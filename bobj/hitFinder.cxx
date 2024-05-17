7b1318a2e42920ca0d1b3afcd582cf86e9c87cd3
liebercanis <mgold@unm.edu>
2024-05-16 18:37:59 -0600

    debug

--- bobj/hitFinder.cxx
+++ bobj/hitFinder.cxx
@@ -937,8 +937,10 @@ void hitFinder::makeHits(int idet, Double_t &triggerTime, Double_t &firstCharge)
 
     detHits.insert(std::pair<Double_t, TDetHit>(hitTime, dhit));
     hPeakNWidth->Fill(dhit.lastBin - dhit.firstBin + 1);
-    if (1)
+    if (1) {
+      dhit.print();
       printf("line941 hitFinder::makeHits %llu insert hit idet %i  time %f (%u,%u) peak bin %i kind %i length %u qpeak %f detHit size %lu  \n", theEvent, idet, hitTime, dhit.firstBin, dhit.lastBin, dhit.peakBin, peakKind[ip], khigh - klow + 1, qpeak, detHits.size());
+    }
   }
 
     int nhit = 0;
