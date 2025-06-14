db129a72cd96044a15c947c69ef4b435b6ed265a
Liebercanis <mgold@unm.edu>
2025-06-14 13:54:07 -0600

    bug fix

--- compiled/summary.cc
+++ compiled/summary.cc
@@ -638,6 +638,7 @@ void fileLoop()
       printf("NO hTrigSumCut IN FILE %s\n", fin->GetName());
       continue;
     }
+    printf("GOT hTrigSumCut IN FILE %s\n", fin->GetName());
     if (ifile == 0)
     {
       hRunTrigSumCut = (TH1D *)hTrigSumCut->Clone("RunTrigSumCut");
