6dbd1191c9fd4886d272f1ec7563d3034c59d2f2
Liebercanis <mgold@unm.edu>
2025-06-30 12:49:34 -0600

    separated out findAndFitPeaks(int i)

--- gain.C
+++ gain.C
@@ -1,12 +1,16 @@
 /*
-   read and fit gains from summary June 24 2024
+   read and fit gains from summary
    updated sept 16 2024
    updated June 14 2025!
+   updated version 2 June 27 2025
 */
 #include <ctime>
 #include <iostream>
 #include "TDirectory.h"
 #include "TGraphErrors.h"
+
+TFile *fin;
+TFile *fout;
 // double nominalGain = 227.4; // average
 double theNominalGain;
 /**************** define nominal gains ***************/
@@ -80,6 +84,53 @@ string currentDate()
   return string(output);
 }
 
+void getHistosFromFile(int theChan)
+{
+
+  // get histos from file
+  TDirectory *gainSumDir = nullptr;
+  fin->GetObject("gainSumDir", gainSumDir);
+
+  //
+  TIter next(gainSumDir->GetListOfKeys());
+  TKey *key;
+  while (TKey *key = (TKey *)next())
+  {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
+
+    if (!cl->InheritsFrom("TH1D"))
+      continue;
+    TH1D *h = (TH1D *)key->ReadObj();
+    TString hname(h->GetName());
+    int ichan = TString(hname(hname.Last('n') + 1, hname.Length())).Atoi();
+
+    // struggle to get only last hist cycle
+    TString lastName;
+    if (hlist.size() > 0)
+      lastName = hlist[hlist.size() - 1]->GetName();
+    // cout << " xxx " << h->GetName() << " " << lastName << endl;
+    if (TString(h->GetName()) == lastName)
+      continue;
+
+    if (ichan > theChan)
+      continue;
+
+    if (ichan > 11)
+      continue;
+
+    /* set the nominal gain */
+    theNominalGain = nominalGain;
+    if (ichan > 8 && ichan < 12)
+      theNominalGain = nominalTrigGain;
+
+    cout << "hist name " << h->GetName() << " chan " << ichan << " cycle " << key->GetCycle() << endl;
+    // TH1D *hclone = (TH1D *)h->Clone();
+    //  fout->Add(hclone);
+    hlist.push_back(h);
+    vchan.push_back(ichan);
+  }
+}
+
 bool readGains(TString fileName)
 {
   TFile *gfin = new TFile(fileName, "readonly");
@@ -128,42 +179,110 @@ bool readGains(TString fileName)
   return true;
 }
 
+// find and fit peaks to a histogram in hlist
+void findAndFitPeaks(int i)
+{
+  TH1D *h = hlist[i];
+  // arrays to hold the fit points
+  fFitADC.clear();
+  fFitADCY.clear();
+  fFitADCError.clear();
+  fSpeNumber.clear();
+  fSpeNumberError.clear();
+
+  // loop over peaks
+  for (unsigned j = 0; j < NPEAKS; ++j)
+  {
+    double width = 30.; // set by eye
+    if (j == 0)
+    {
+      theNominalGain = nominalGain;
+      if (vchan[i] > 8 && vchan[i] < 12)
+      { // trigger
+        width = 100.0;
+        theNominalGain = nominalTrigGain;
+      }
+    }
+
+    // peak search region
+    double nominalPeak = theNominalGain * double(j + 1);
+    double fitStart = theNominalGain * double(j + 1) - 3. * width;
+    double fitEnd = theNominalGain * double(j + 1) + 3. * width;
+    // find true peak
+    int bin = findPeakBin(h, fitStart, fitEnd);
+    double xbin = h->GetBinLowEdge(bin);
+    double peakVal = h->GetBinContent(bin);
+    // redefine fit limits about true peak
+    fitStart = xbin - width;
+    fitEnd = xbin + width;
+    // dont fit peaks too small
+    if (peakVal < 3)
+      continue;
+    // fit here
+    // h->GetListOfFunctions()->ls();
+    h->GetListOfFunctions()->Clear();
+    //  printf("fit to hist %s peak # %u bin %i xbin %f val %f from %f to %f \n", h->GetName(), j + 1, bin, xbin, peakVal, fitStart, fitEnd);
+    h->Fit("gaus", "QS+", "", fitStart, fitEnd);
+    TF1 *gFit = (TF1 *)h->GetListOfFunctions()->FindObject("gaus");
+    if (gFit != nullptr)
+    {
+      vfit[i].push_back(gFit);
+      // scale to nominal
+      double mean = gFit->GetParameter(1);
+      double meanError = gFit->GetParError(1);
+      int jbin = h->FindBin(mean);
+      double val = h->GetBinContent(jbin);
+      printf("fit to hist %s nominalPeak %f width %f point %u  bin %i nominal x %.2f from %.0f to %.0f  mean %.2f error %.2f peak bin %i val %.2f  \n",
+             h->GetName(), nominalPeak, width, j, bin, xbin, fitStart, fitEnd, mean, meanError, jbin, val);
+      fFitADC.push_back(mean);
+      fFitADCY.push_back(val);
+      fFitADCError.push_back(meanError);
+      fSpeNumber.push_back(j + 1);
+      fSpeNumberError.push_back(0);
+      printf("\t result of fit to hist %i point %u  bin %i x %f xadc %f  \n", i, j, bin, fFitADC[j], fFitADCY[j]);
+      // update nomainl gain from first peak
+      if (j == 0)
+        theNominalGain = mean;
+    }
+    else
+    {
+      printf("FIT FAILS to hist %s peak # %u bin %i xbin %f val %f from %f to %f \n", h->GetName(), j + 1, bin, xbin, peakVal, fitStart, fitEnd);
+    }
+  } // loop over peaks
+}
+
 // Define a linear fit
 double fline(double *x, double *par)
 {
   return par[0] + x[0] * par[1];
 }
 
-/* main */
+/***********************
+ * macro main entry
+ * ******************* */
 void gain(int theChan = 13) // default all
 {
   sdate = currentDate();
   printf(" making cans on %s \n", sdate.c_str());
-  TString tag0;
-  TString tag1;
-  TString tagDate;
-  // tag0=TString("01_25_2024");
-  // tag1=TString("01_25_2024");
-  tag0 = TString("05_19_2025");
-  tag1 = TString("05_19_2025");
-  tagDate = TString("2025-06-16-15-16");
-  int nfiles = 28;
-  TString tagNfiles;
-  tagNfiles.Form("--nfiles-%i", nfiles);
 
-  TString fileName = TString("summary-") + tag0 + TString("--") + tag1 + tagNfiles + TString("-created-") + tagDate + TString(".root");
-  cout << " gains from file " << fileName << endl;
+  // put in explicit file name and get tag
+  TString fileName("summary-05_19_2025-05_19_2025-nfiles-31-created-2025-06-27-11-49.root");
+  TString tag = TString(fileName(fileName.First("-") + 1, 21));
+  cout << " gains from file " << fileName << " with date tag " << tag << endl;
 
-  // TH1D *hFit = new TH1D("GausFit", "GausFit", 4000, -20.E3, 200.E3);
-  // TFile *fin = new TFile("summary-12_28_2023-01_25_2024-nfiles-45-created-2024-06-27-14-43.root", "readonly");
-  TFile *fin = new TFile(fileName, "readonly");
+  // open file
+  fin = new TFile(fileName, "readonly");
   if (fin->IsZombie())
     return;
+
+  // open output file
   std::string sdate = currentDate();
-  TFile *fout = new TFile(Form("gains-%s.root", sdate.c_str()), "recreate");
+  fout = new TFile(Form("gains--%s-%s.root", tag.Data(), sdate.c_str()), "recreate");
+
+  // function to fit line
   TF1 *line = new TF1("myLine", fline, 0, 2.E5, 2);
 
-  // read old gain file
+  // read old gain file for comparison
   TString savedGainTag = TString("gains-2024-02-15-17-26-alpha");
   TString gainFileName = TString(getenv("BOBJ")) + savedGainTag + TString(".root");
   printf("read gains from file %s \n", gainFileName.Data());
@@ -178,126 +297,25 @@ void gain(int theChan = 13) // default all
   gSavedGain->SetTitle(Form("saved gains %s ", savedGainTag.Data()));
   fout->Append(gSavedGain);
 
-  // get histos from file
-  TDirectory *gainSumDir = nullptr;
-  fin->GetObject("gainSumDir", gainSumDir);
+  getHistosFromFile(theChan);
 
-  //
-  TIter next(gainSumDir->GetListOfKeys());
-  TKey *key;
-  while (TKey *key = (TKey *)next())
-  {
-    TClass *cl = gROOT->GetClass(key->GetClassName());
-
-    if (!cl->InheritsFrom("TH1D"))
-      continue;
-    TH1D *h = (TH1D *)key->ReadObj();
-    TString hname(h->GetName());
-    int ichan = TString(hname(hname.Last('n') + 1, hname.Length())).Atoi();
-
-    // struggle to get only last hist cycle
-    TString lastName;
-    if (hlist.size() > 0)
-      lastName = hlist[hlist.size() - 1]->GetName();
-    // cout << " xxx " << h->GetName() << " " << lastName << endl;
-    if (TString(h->GetName()) == lastName)
-      continue;
-    if (ichan > 11)
-      continue;
-
-    if (ichan > theChan)
-      continue;
-
-    if (ichan > 11)
-      continue;
-
-    /* set the nominal gain */
-    theNominalGain = nominalGain;
-    if (ichan > 8 && ichan < 12)
-      theNominalGain = nominalTrigGain;
-
-    cout << "hist name " << h->GetName() << " chan " << ichan << " cycle " << key->GetCycle() << endl;
-    // TH1D *hclone = (TH1D *)h->Clone();
-    //  fout->Add(hclone);
-    hlist.push_back(h);
-    vchan.push_back(ichan);
-  }
-
-  printf(" have %lu gain hists \n", hlist.size());
+  printf("line302  have %lu gain hists \n", hlist.size());
   if (hlist.size() == 0)
     return;
 
   // fit peaks
-  vfit.resize(hlist.size());
-
+  vfit.resize(hlist.size()); // store fit to peak
+  // store gain fits to line;
   sipmGain.clear();
   sipmGainError.clear();
+  // loop over histograms
   for (unsigned i = 0; i < hlist.size(); ++i)
   {
-    fFitADC.clear();
-    fFitADCY.clear();
-    fFitADCError.clear();
-    fSpeNumber.clear();
-    fSpeNumberError.clear();
-
-    for (unsigned j = 0; j < NPEAKS; ++j)
-    {
-      TH1D *h = hlist[i];
-      double width = 30.; // set by eye
-      if (j == 0)
-      {
-        theNominalGain = nominalGain;
-        if (vchan[i] > 8 && vchan[i] < 12)
-        { // trigger
-          width = 100.0;
-          theNominalGain = nominalTrigGain;
-        }
-      }
-
-      // peak search region
-      double nominalPeak = theNominalGain * double(j + 1);
-      double fitStart = theNominalGain * double(j + 1) - 3. * width;
-      double fitEnd = theNominalGain * double(j + 1) + 3. * width;
-      // find true peak
-      int bin = findPeakBin(h, fitStart, fitEnd);
-      double xbin = h->GetBinLowEdge(bin);
-      double peakVal = h->GetBinContent(bin);
-      // redefine fit limits about true peak
-      fitStart = xbin - width;
-      fitEnd = xbin + width;
-      // dont fit peaks too small
-      if (peakVal < 3)
-        continue;
-      // fit here
-      h->GetListOfFunctions()->Clear();
-      // printf("fit to hist %s peak # %u bin %i xbin %f val %f from %f to %f \n", h->GetName(), j + 1, bin, xbin, peakVal, fitStart, fitEnd);
-      h->Fit("gaus", "QQSS", "", fitStart, fitEnd);
-      TF1 *gFit = (TF1 *)h->GetListOfFunctions()->FindObject("gaus");
-      if (gFit != nullptr)
-      {
-        vfit[i].push_back(gFit);
-        // scale to nominal
-        double mean = gFit->GetParameter(1);
-        double meanError = gFit->GetParError(1);
-        int jbin = h->FindBin(mean);
-        double val = h->GetBinContent(jbin);
-        printf("fit to hist %s nominalGain %f width %f point %u  bin %i nominal x %.2f from %.0f to %.0f  mean %.2f error %.2f peak bin %i val %.2f  \n",
-               h->GetName(), theNominalGain, width, j, bin, xbin, fitStart, fitEnd, mean, meanError, jbin, val);
-        fFitADC.push_back(mean);
-        fFitADCY.push_back(val);
-        fFitADCError.push_back(meanError);
-        fSpeNumber.push_back(j + 1);
-        fSpeNumberError.push_back(0);
-        printf("\t result of fit to hist %i point %u  bin %i x %f xadc %f  \n", i, j, bin, fFitADC[j], fFitADCY[j]);
-        // update nomainl gain from first peak
-        if (j == 0)
-          theNominalGain = mean;
-      }
-      else
-      {
-        printf("FIT FAILS to hist %s peak # %u bin %i xbin %f val %f from %f to %f \n", h->GetName(), j + 1, bin, xbin, peakVal, fitStart, fitEnd);
-      }
-    } // loop over peaks
+    if (!TString(hlist[i]->GetName()).Contains("Peak"))
+      continue;
+    printf(" find and fit %s ", hlist[i]->GetName());
+    findAndFitPeaks(i);
+    fout->Add(hlist[i]);
 
     printf(" FFFFFFF fit for channel %i n= %lu \n", i, fFitADC.size());
     for (unsigned long j = 0; j < fFitADC.size(); ++j)
@@ -307,27 +325,7 @@ void gain(int theChan = 13) // default all
     if (fFitADC.size() < 2)
       continue;
 
-    /*****
-    // add marker
-    TPolyMarker *pmold = (TPolyMarker *)hlist[i]->GetListOfFunctions()->FindObject("TPolyMarker");
-    if (pmold)
-    {
-      hlist[i]->GetListOfFunctions()->Remove(pmold);
-      delete pmold;
-    }
-
-    TPolyMarker *pm = new TPolyMarker(fFitADC.size(), &fFitADC[0], &fFitADCY[0]);
-    hlist[i]->GetListOfFunctions()->Add(pm);
-    double *xp = pm->GetX();
-    double *yp = pm->GetY();
-    for (int ip = 0; ip < pm->GetN(); ++ip)
-      printf("POLY det %i poly %i %f %f \n", i, ip, xp[ip], yp[ip]);
-    pm->SetMarkerStyle(23);
-    pm->SetMarkerColor(kRed);
-    pm->SetMarkerSize(1.5);
-    */
-
-    // make graph
+    // make graph from fit points
     TGraphErrors *g = new TGraphErrors(fFitADC.size(), &fSpeNumber[0], &fFitADC[0], &fSpeNumberError[0], &fFitADCError[0]);
     // g->Print();
     g->SetMarkerStyle(23);
@@ -341,6 +339,8 @@ void gain(int theChan = 13) // default all
     g->SetName(gname);
     g->SetTitle(gtitle.Data());
     line->SetParameters(0.5, 0);
+
+    /** fit to line slope is the gain */
     g->Fit("myLine");
     // g->GetHistogram()->GetListOfFunctions()->ls();
     TF1 *gFit = g->GetFunction("myLine");
@@ -435,7 +435,7 @@ void gain(int theChan = 13) // default all
 
   // plot
   TString graphTitle;
-  graphTitle = Form("gain variation %s to %s ", tag0.Data(), tag1.Data());
+  graphTitle = Form("gain variation %s ", tag.Data());
 
   for (int is = 0; is < sipmGain.size(); ++is)
   {
@@ -460,8 +460,8 @@ void gain(int theChan = 13) // default all
 
   TString canTitle;
   TString canName;
-  canName = Form("gainVariation-%s-%s", tag0.Data(), tag1.Data());
-  canTitle = Form("gain variation %s to %s ", tag0.Data(), tag1.Data());
+  canName = Form("gainVariation-%s", tag.Data());
+  canTitle = Form("gain variation %s ", tag.Data());
   TCanvas *canGain = new TCanvas(canName, canTitle);
   cout << "line388  " << canGain->GetName() << endl;
   gPad->SetGrid();
@@ -488,9 +488,9 @@ void gain(int theChan = 13) // default all
 
   TGraphErrors *absGain = new TGraphErrors(absoluteGain.size(), &sipmNumber[0], &absoluteGain[0], &sipmNumberError[0], &absoluteGainError[0]);
   TString absoluteName;
-  absoluteName = Form("gains-%s-%s", tag0.Data(), tag1.Data());
+  absoluteName = Form("gains-%s", tag.Data());
   TString absoluteTitle;
-  absoluteTitle = Form("absolute gain  %s to %s ", tag0.Data(), tag1.Data());
+  absoluteTitle = Form("absolute gain  %s", tag.Data());
   absGain->SetName(absoluteName);
   absGain->SetTitle(absoluteTitle);
   absGain->GetHistogram()->GetYaxis()->SetTitle("absolute gain");
@@ -502,8 +502,8 @@ void gain(int theChan = 13) // default all
   absGain->GetHistogram()->GetYaxis()->SetTitle("gain ADC/PE");
   absGain->GetHistogram()->GetXaxis()->SetTitle("channel");
 
-  canName = Form("absoluteGain-%s-%s", tag0.Data(), tag1.Data());
-  canTitle = Form("absolute gain  %s to %s ", tag0.Data(), tag1.Data());
+  canName = Form("absoluteGain-%s", tag.Data());
+  canTitle = Form("absolute gain  %s ", tag.Data());
   TCanvas *canAbsGain = new TCanvas(canName, canTitle);
   gPad->SetGrid();
   absGain->Draw("AP");
