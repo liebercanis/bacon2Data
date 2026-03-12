d4a42e78b229909825a8861b2aa6b00df7bce150
Liebercanis <mgold@unm.edu>
2026-03-12 15:28:59 -0600

    tweaks

--- compiled/submit_postAna.sh
+++ compiled/submit_postAna.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=anacDir
 #SBATCH -N 1
 #SBATCH -q shared
-#SBATCH --qos=debug 
 #SBATCH --constraint=cpu
 #SBATCH --constraint=cpu #user regular for production, debug for testing
 #SBATCH --time=02:00:00
@@ -12,7 +11,6 @@
 #SBATCH --mem=1GB
 #SBATCH --output=logs/postAna_%j.log
 #SBATCH --error=logs/postAna_%j.err
-#SBATCH --array=0-31%8
 #SBATCH --mail-user=mgold@unm.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m2676
