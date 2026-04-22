9a8b4b73f01b7e3115bfd1020a876787e76dc657
Liebercanis <mgold@unm.edu>
2026-04-22 12:58:10 -0600

    32 gig mem

--- submit_slurm.sh
+++ submit_slurm.sh
@@ -3,11 +3,11 @@
 #SBATCH -N 1
 #SBATCH -q shared
 #SBATCH -C cpu
-#SBATCH --time=00:30:00
+#SBATCH --time=03:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1GB
+#SBATCH --mem=32GB
 #SBATCH --output=logs/anaDir_%j.log
 #SBATCH --error=logs/anacDir_%j.err
 #SBATCH --array=0-31%8
