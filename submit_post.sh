fdcb5512681bb32377a7a065a4551a756f8a4491
Liebercanis <mgold@unm.edu>
2026-05-08 13:30:24 -0600

    debug

--- submit_post.sh
+++ submit_post.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=postAna_mgold
 #SBATCH -N 1
-#SBATCH -q shared
-#SBATCH -C cpu
+#SBATCH -q=shared
+#SBATCH -C=cpu
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH -q=shared
-#SBATCH  -C=cpu
 #SBATCH --mem=32GB
 #SBATCH --output=logs/postAna_%j.log
 #SBATCH --error=logs/postAna_%j.err
