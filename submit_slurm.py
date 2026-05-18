510b3f4931e59aa3c9fca7054a1388afb3da8a86
Liebercanis <mgold@unm.edu>
2026-05-18 14:09:28 -0600

    remove single job

--- submit_slurm.py
+++ submit_slurm.py
@@ -61,7 +61,7 @@ def submit_slurm_job(args):
         f'--array=0-{num_tasks-1}%{args.parallel}',
         f'--mem={args.mem}',
         f'--time={args.time_limit}',
-        f'--date-tag={args.date_tag}',
+        f'--date_tag={args.date_tag}',
         f'-A {args.account}',
         f'-q {args.queue}',
         f'-C', 'cpu',
