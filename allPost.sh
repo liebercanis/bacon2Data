60e6a36a6bcd826d9ce9c9a5011011fd90310799
Michael Gold <mgold@unm.edu>
2026-06-08 15:03:30 -0600

    submit all post jobs

--- allPost.sh
+++ allPost.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-submit_slurm.py 04_16_2026
-submit_slurm.py 04_24_2026
-submit_slurm.py 04_28_2026
-submit_slurm.py 04_30_2026
-submit_slurm.py 05_01_2026
-submit_slurm.py 05_03_2026
-submit_slurm.py 05_05_2026
-submit_slurm.py 05_07_2026
-submit_slurm.py 05_09_2026
-submit_slurm.py 05_11_2026
-submit_slurm.py 05_14_2026
-submit_slurm.py 05_18_2026
+sbatch submit_post.sh 04_16_2026
+sbatch submit_post.sh 04_24_2026
+sbatch submit_post.sh 04_28_2026
+sbatch submit_post.sh 04_30_2026
+sbatch submit_post.sh 05_01_2026
+sbatch submit_post.sh 05_03_2026
+sbatch submit_post.sh 05_05_2026
+sbatch submit_post.sh 05_07_2026
+sbatch submit_post.sh 05_09_2026
+sbatch submit_post.sh 05_11_2026
+sbatch submit_post.sh 05_14_2026
+sbatch submit_post.sh 05_18_2026
