dc2fc2facfcfbbf2ce3ad36d1162f4c19a65eb27
Michael Gold <mgold@unm.edu>
2026-06-12 10:05:53 -0600

    copy post from perl

---
+++ compiled/copyPost.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+export COPY="scp -i ~/.ssh/nersc mgold@perlmutter.nersc.gov:"
+echo "COPY wth " $COPY
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-04_16_2026-04_16_2026-10281297.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-04_24_2026-04_24_2026-10341507.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-04_30_2026-04_30_2026-6910781.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_18_2026-05_18_2026-7107472.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_03_2026-05_03_2026-10135057.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-04_28_2026-04_28_2026-11344902.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_01_2026-05_01_2026-10587911.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_07_2026-05_07_2026-10063119.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_09_2026-05_09_2026-11006186.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_05_2026-05_05_2026-12500971.root . 
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_14_2026-05_14_2026-10233851.root .
+$COPY/global/homes/m/mgold/mgold/bacon2Data/compiled/post-05_11_2026-05_11_2026-12249461.root .
+
