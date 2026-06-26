a79e1260df52700decf5b497bc55dc41dc4ed9ec
Michael Gold <mgold@unm.edu>
2026-06-26 14:14:56 -0600

    rum all postAllFile

---
+++ compiled/postAllFile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+root -b -q "postMacroAllFile.C(0)"
+root -b -q "postMacroAllFile.C(1)"
+root -b -q "postMacroAllFile.C(2)"
+root -b -q "postMacroAllFile.C(3)"
+root -b -q "postMacroAllFile.C(4)"
+root -b -q "postMacroAllFile.C(5)"
+root -b -q "postMacroAllFile.C(6)"
+root -b -q "postMacroAllFile.C(7)"
+root -b -q "postMacroAllFile.C(8)"
+root -b -q "postMacroAllFile.C(9)"
+root -b -q "postMacroAllFile.C(10)"
+root -b -q "postMacroAllFile.C(11)"
+
